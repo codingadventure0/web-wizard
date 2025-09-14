3c5b90d6f54d4a1aa15ad47c8e41653fada2722d
Deepak <deepakmahtojms7@gmail.com>
2025-09-14 12:03:06 +0530

    add nav & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import Hero from "./component/Hero";
 import "./App.css";
-<<<<<<< HEAD
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
-=======
->>>>>>> 40be5d1ff0381535326ea5f4e1b0816341dd9bd4
 
 function App() {
   const [count, setCount] = useState(0);
